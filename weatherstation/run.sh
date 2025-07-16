1c61efca5367c21804b23c32e767f938f635b8ae
Bewo <bernd@worlitschek.eu>
2025-07-16 22:53:40 +0200

    Update run.sh

--- weatherstation/run.sh
+++ weatherstation/run.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/with-contenv bashio
+
+export MQTT_HOST=$(bashio::config 'mqtt_host')
+export MQTT_PORT=$(bashio::config 'mqtt_port')
+export MQTT_USER=$(bashio::config 'mqtt_user')
+export MQTT_PASSWORD=$(bashio::config 'mqtt_password')
+export MQTT_TOPIC=$(bashio::config 'mqtt_topic')
+
 echo "Start Server for Bresser Weatherstation"
 /opt/venv/bin/python /weather_server.py
